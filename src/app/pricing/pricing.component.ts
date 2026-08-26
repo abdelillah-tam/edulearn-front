33139a63d1eedb458a7508dfa037074e6406c0b2
unknown <atamossat@gmail.com>
2026-08-26 19:15:04 +0100

    built monthly and annual subscriptions elements and designed them

--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-pricing',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './pricing.component.html',
   styleUrl: './pricing.component.css',
 })
 export class PricingComponent {
+  starter = [
+    'Access to 500+ courses',
+    'Course completion certificates',
+    'Downloadable resources',
+    'Email support',
+    'Community forum access',
+  ];
 
+  pro = [
+    'Access to 500+ courses',
+    'Course completion certificates',
+    'Downloadable resources',
+    'Priority 24/7 support',
+    'Community forum access',
+    'Lifetime access to purchased courses',
+    'Early access to new courses',
+    'Exclusive mentorship sessions',
+  ];
 }
